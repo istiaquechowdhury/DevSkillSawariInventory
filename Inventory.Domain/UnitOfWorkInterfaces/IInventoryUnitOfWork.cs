d5544cf45ca5b8ed59d19ae4797487794fa7b369
istiaquechowdhury <istiaquechowdhury430@gmail.com>
2024-11-25 23:25:58 +0600

    Complete Project

---
+++ Inventory.Domain/UnitOfWorkInterfaces/IInventoryUnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using Inventory.Domain.Dtos;
+using Inventory.Domain.Entities;
+using Inventory.Domain.RepositoryPatternInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Domain.UnitOfWorkInterfaces
+{
+    public interface IInventoryUnitOfWork : IUnitOfWork   
+    {
+        public IProductRepository Product { get; }   
+
+        public ICategoryRepository Category { get; }
+
+        public ITaxRepository Tax { get; }
+
+        public IWareHouseRepository WareHouse { get; }
+
+        public IStockListRepository StockList { get; }
+
+        public IStockTransferRepository StockTransfer { get; }
+
+        public IStockAdjustmentRepository StockAdjustment { get; }
+
+        public IReasonRepository Reason { get; }    
+
+        public IMeasurementRepository Measurement { get; }
+
+        Task<(IList<ProductDto> data, int total, int totaldisplay)> GetPagedProductsUsingSPAsync(int pageIndex, int pageSize, ProductSearchDto search, string? order);
+        Task<(IList<StockListDto> data, int total, int totaldisplay)> GetPagedStocklistUsingSPAsync(int pageIndex, int pageSize, StockListSearchDto search, string? order);
+
+    }
+}
--- README.md
+++ README.md
@@ -1,11 +1,1 @@
-
-Sawari - Inventory Management System        
-Developed a robust inventory management system for efficient stock management and adjustments.
-
-Key features include:
-
-- Built with ASP.NET Core MVC 8, allowing administrators to manage stock items, transfers, and adjustments.
-- Enabled exclusive access for administrators to add, transfer, and adjust stock, enhancing control over inventory tracking.
-- Implemented a stock list feature for a clear overview of available inventory items.
-- Fully dockerized for seamless deployment and optimized resource management.
-- Emphasized scalability and maintainability with modern architectural practices and secure design.
+# Aspnet-B10
